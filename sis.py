e4d6d6cda589ca36e4f9a0874e17c12cb77fb723
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2023-05-25 15:32:40 -0300

    add association for names

--- sis.py
+++ sis.py
@@ -1,29 +1,55 @@
+import pickle
+import random
 
 # Linha 0: nomes
 # Linha 1: cpf
-# Linha 3: números
+# Linha 2: telefones
+# Linha 3: data
+# Linha 4: hora
 
-matriz = [["eduarda", "eduardo"],
-        ["124.982.904-51", "568,365.704-36"],
-          ["9996985245", "7854254421582698"]]
+matriz = [[], # Matriz de nomes 
+          [], # Matriz de CPFs
+          [], # Matriz de telefones
+          [], # Matriz de datas
+          []] # Matriz de hora
+
+def carregar_dados():
+    try:
+        with open('dados.pkl', 'rb') as arquivo:
+            matriz = pickle.load(arquivo)
+    except FileNotFoundError:
+        matriz = [[], [], [], [], []]
+    return matriz
+
+def salvar_dados(matriz):
+    with open('dados.pkl', 'wb') as arquivo:
+        pickle.dump(matriz, arquivo)
 
 def cadastro(matriz):
-    nome = input("Informe seu nomes: ")
+    nome = input("Informe seu nome: ")
     matriz[0].append(nome)
     cpf = input("Informe seu cpf: ")
-    matriz[1].append(cpf)  
+    matriz[1].append(cpf)
     telefone = input("Informe seu telefone: ")
     matriz[2].append(telefone)
 
+def cadastra_data_hora(matriz, nome):
+    index = matriz[0].index(nome)
+    dias_semana = ["segunda", "terça", "quarta", "quinta", "sexta"]
+    dia = random.choice(dias_semana)
+    matriz[3].append(dia)
+
+def get_consulta(matriz, nome):
+    index = matriz[0].index(nome)
+    dia_consulta = matriz[3][index]
+    return dia_consulta
+
+# Carrega os dados salvos, se existirem
+matriz = carregar_dados()
+
 cadastro(matriz)
-print(matriz[0][2])
-print(matriz[1][2])
-print(matriz[2][2])
+cadastra_data_hora(matriz, "Eduardo")
+print(get_consulta(matriz, "Eduardo"))
 
-
-
-        
-
-
-       
-
+# Salva os dados atualizados
+salvar_dados(matriz)
